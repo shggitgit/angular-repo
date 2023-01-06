c831a418a9151e1c9b158b56af1e2a899bd4dad0
unknown <pshega@gmail.com>
2023-01-06 15:00:38 -0500

    updated buildscript file

--- testApp/buildproject.sh
+++ testApp/buildproject.sh
@@ -1,10 +1,8 @@
-sh -exc | env
-sh -exc | ls -lrth
-sh -exc | cd myrepo
-sh -exc | ls -lrth
-sh -exc | cd testApp
-sh -exc | ls -lrth
-sh -exc | npm install
-sh -exc | ng build
-sh -exc | cd dist
-sh -exc | ls -lrth
+sh -exc | 
+env
+ls -lrth
+cd myrepo
+ls -lrth
+cd testApp
+ls -lrth
+
